8c1e6c7efdd00476bd5347bdf9e731140c8ed110
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-05-25 19:36:16 +0700

    Implement ViewBinding

--- app/src/main/java/com/rujirakongsomran/kt_materialtimepicker/FirstFragment.kt
+++ app/src/main/java/com/rujirakongsomran/kt_materialtimepicker/FirstFragment.kt
@@ -5,15 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.rujirakongsomran.kt_materialtimepicker.databinding.FragmentFirstBinding
 
 class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
