dc451ffc4748c6a52c34950ee9c1ff0e6f87315d
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-05-25 21:45:02 +0700

    Paste fragment on activity

--- app/src/main/java/com/rujirakongsomran/kt_materialtimepicker/FirstFragment.kt
+++ app/src/main/java/com/rujirakongsomran/kt_materialtimepicker/FirstFragment.kt
@@ -1,10 +1,13 @@
 package com.rujirakongsomran.kt_materialtimepicker
 
 import android.os.Bundle
+import android.text.format.DateFormat.is24HourFormat
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
 import com.rujirakongsomran.kt_materialtimepicker.databinding.FragmentFirstBinding
 
 class FirstFragment : Fragment() {
@@ -18,7 +21,25 @@ class FirstFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
+
+        binding.button.setOnClickListener {
+            openTimePicker()
+        }
+
         return binding.root
+    }
+
+    private fun openTimePicker() {
+        val isSystem24Hour = is24HourFormat(requireContext())
+        val clockFormat = if (isSystem24Hour) TimeFormat.CLOCK_24H else TimeFormat.CLOCK_12H
+
+        val picker = MaterialTimePicker.Builder()
+            .setTimeFormat(clockFormat)
+            .setHour(12)
+            .setMinute(0)
+            .setTitleText("Set Alarm")
+            .build()
+        picker.show(childFragmentManager, "TAG")
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/rujirakongsomran/kt_materialtimepicker/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/kt_materialtimepicker/MainActivity.kt
@@ -7,5 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val firstFragment = FirstFragment()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container_view, firstFragment)
+            .commit()
     }
 }
